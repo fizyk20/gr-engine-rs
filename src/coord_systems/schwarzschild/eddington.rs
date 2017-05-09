69b89bfb6052bb994e63f9e7e41422d56baf20b2
Bartłomiej Kamiński <fizyk20@gmail.com>
2017-05-09 22:55:37 +0200

    Add Christoffel symbols for EF coords in Schwarzschild spacetime

--- src/coord_systems/schwarzschild/eddington.rs
+++ src/coord_systems/schwarzschild/eddington.rs
@@ -1,7 +1,7 @@
 use typenum::consts::U4;
 use diffgeom::coordinates::{CoordinateSystem, Point};
 use diffgeom::metric::MetricSystem;
-use diffgeom::tensors::{TwoForm, InvTwoForm};
+use diffgeom::tensors::{CovariantIndex, ContravariantIndex, TwoForm, InvTwoForm, Tensor};
 use super::Mass;
 use std::marker::PhantomData;
 
@@ -44,8 +44,38 @@ impl<M: Mass> MetricSystem for EddingtonFinkelstein<M> {
         ])
     }
 
+    fn christoffel(x: &Point<Self>)
+                   -> Tensor<Self, (ContravariantIndex, (CovariantIndex, CovariantIndex))> {
+        let u = x[0];
+        let r = x[1];
+        let th = x[2];
+        let ph = x[3];
+        let m = M::mass();
+        Tensor::<Self, (ContravariantIndex, (CovariantIndex, CovariantIndex))>::new(x.clone(),
+                                                                                    arr![f64;
+            m/r/r, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, -r, 0.0,
+            0.0, 0.0, 0.0, -r*th.sin()*th.sin(),
+
+            m*(1.0-2.0*m/r)/r/r, -m/r/r, 0.0, 0.0,
+            -m/r/r, 0.0, 0.0, 0.0,
+            0.0, 0.0, -(r-2.0*m), 0.0,
+            0.0, 0.0, 0.0, -(r-2.0*m)*th.sin()*th.sin(),
+
+            0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 1.0/r, 0.0,
+            0.0, 1.0/r, 0.0, 0.0,
+            0.0, 0.0, 0.0, -th.sin()*th.cos(),
+
+            0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 1.0/r,
+            0.0, 0.0, 0.0, th.cos()/th.sin(),
+            0.0, 1.0/r, th.cos()/th.sin(), 0.0
+        ])
+    }
+
     // TODO
     //fn dg(x: &Point<Self>) -> Tensor<Self, (CovariantIndex, (CovariantIndex, CovariantIndex))>
     //fn covariant_christoffel(x: &Point<Self>) -> Tensor<Self, (CovariantIndex, (CovariantIndex, CovariantIndex))>
-    //fn christoffel(x: &Point<Self>) -> Tensor<Self, (ContravariantIndex, (CovariantIndex, CovariantIndex))>
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// Needed for Christoffel symbols in arr! macro
+// (64 numbers in a 4D spacetime)
+#![recursion_limit="70"]
+
 #[macro_use]
 extern crate generic_array;
 extern crate diffgeom;
