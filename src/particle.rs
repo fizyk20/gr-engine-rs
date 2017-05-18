bd2bf791a07f4bfbbe373e3db3e76aecadfd9d29
Bartłomiej Kamiński <fizyk20@gmail.com>
2017-05-18 21:33:35 +0200

    Simplified the Particle implementation

--- src/particle.rs
+++ src/particle.rs
@@ -3,6 +3,7 @@ use diffgeom::metric::MetricSystem;
 use diffgeom::tensors::Vector;
 use generic_array::{ArrayLength, GenericArray};
 use numeric::StateVector;
+use numeric_algs::State;
 use numeric_algs::integration::DiffEq;
 use std::ops::Mul;
 use typenum::{Exp, Pow, Prod, Unsigned};
@@ -16,38 +17,41 @@ struct Particle<C: CoordinateSystem>
     v: Vector<C>,
 }
 
-impl<C: CoordinateSystem> From<Particle<C>> for StateVector<Prod<C::Dimension, U2>>
-    where C::Dimension: Pow<U1> + Mul<U2> + Unsigned,
-          Exp<C::Dimension, U1>: ArrayLength<f64>,
-          Prod<C::Dimension, U2>: ArrayLength<f64>
+impl<C: CoordinateSystem> Clone for Particle<C>
+    where C::Dimension: Pow<U1>,
+          Exp<C::Dimension, U1>: ArrayLength<f64>
 {
-    fn from(particle: Particle<C>) -> Self {
-        let mut result = GenericArray::default();
-        let d = C::Dimension::to_usize();
-        for i in 0..d {
-            result[i] = particle.x[i];
+    fn clone(&self) -> Self {
+        Particle {
+            x: self.x.clone(),
+            v: self.v.clone(),
         }
-        for i in 0..d {
-            result[i + d] = particle.v[i];
-        }
-        StateVector(result)
     }
 }
 
-impl<C: CoordinateSystem> From<StateVector<Prod<C::Dimension, U2>>> for Particle<C>
+impl<C: CoordinateSystem> State for Particle<C>
     where C::Dimension: Pow<U1> + Mul<U2> + Unsigned,
           Exp<C::Dimension, U1>: ArrayLength<f64>,
-          Prod<C::Dimension, U2>: ArrayLength<f64>
+          Prod<C::Dimension, U2>: ArrayLength<f64>,
+          <Prod<C::Dimension, U2> as ArrayLength<f64>>::ArrayType: Copy
 {
-    fn from(state_vec: StateVector<Prod<C::Dimension, U2>>) -> Self {
+    type Derivative = StateVector<Prod<C::Dimension, U2>>;
+
+    fn shift(&self, dir: Self::Derivative, amount: f64) -> Self {
         let d = C::Dimension::to_usize();
-        let x = Point::from_slice(&state_vec.0[0..d]);
-        let v = Vector::from_slice(x.clone(), &state_vec.0[d..d * 2]);
+        let mut new_x = GenericArray::default();
+        let mut new_v = GenericArray::default();
+        for i in 0..d {
+            new_x[i] = self.x[i] + dir.0[i] * amount;
+            new_v[i] = self.v[i] + dir.0[i + d] * amount;
+        }
+        let x = Point::new(new_x);
+        let v = Vector::new(x.clone(), new_v);
         Particle { x: x, v: v }
     }
 }
 
-impl<C: CoordinateSystem> DiffEq<StateVector<Prod<C::Dimension, U2>>> for Particle<C>
+impl<C: CoordinateSystem> DiffEq<Particle<C>> for Particle<C>
     where C::Dimension: Pow<U1> + Mul<U2> + Unsigned + Pow<U2> + Pow<U3>,
           Exp<C::Dimension, U1>: ArrayLength<f64>,
           Prod<C::Dimension, U2>: ArrayLength<f64>,
@@ -56,19 +60,14 @@ impl<C: CoordinateSystem> DiffEq<StateVector<Prod<C::Dimension, U2>>> for Partic
           Exp<C::Dimension, U2>: ArrayLength<f64>,
           Exp<C::Dimension, U3>: ArrayLength<f64>
 {
-    fn derivative(&self,
-                  state: StateVector<Prod<C::Dimension, U2>>)
-                  -> StateVector<Prod<C::Dimension, U2>> {
-        let particle = Particle::<C>::from(state);
-        let christoffel = C::christoffel(&particle.x);
-        let temp = inner!(_, Vector<C>; U1, U3; christoffel, particle.v.clone());
-        let cov_der = inner!(_, Vector<C>; U1, U2; temp, particle.v.clone());
+    fn derivative(&self, state: Particle<C>) -> StateVector<Prod<C::Dimension, U2>> {
+        let christoffel = C::christoffel(&state.x);
+        let temp = inner!(_, Vector<C>; U1, U3; christoffel, state.v.clone());
+        let cov_der = inner!(_, Vector<C>; U1, U2; temp, state.v.clone());
         let mut result = GenericArray::default();
         let d = C::Dimension::to_usize();
         for i in 0..d {
-            result[i] = particle.v[i];
-        }
-        for i in 0..d {
+            result[i] = state.v[i];
             result[i + d] = -cov_der[i];
         }
         StateVector(result)
